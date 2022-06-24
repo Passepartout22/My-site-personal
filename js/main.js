5473febcec721029ed5ca89d9d2484c6ba06253a
Zeus Cach <99676255+Passepartout22@users.noreply.github.com>
2022-06-24 14:05:08 -0500

    terminando programacion de idioma

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const mode = document.getElementById('mode');
 const body = document.body;
 
+
 load();
 
 mode.addEventListener('click', e => {
      body.classList.toggle('dark');
      store( body.classList.contains('dark'))
 });
+
+function idioma_en() {
+    location.href="en/index.html";
+}
+
+function idioma_es() {
+    location.href="../index.html";
+}
 
 function load() {
     const darkmode = localStorage.getItem('dark');
